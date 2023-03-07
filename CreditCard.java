40cb73a20a61da6197190d381ad86be55b0056d0
Ngufuli <ngufuli@outlook.com>
2023-03-07 14:22:54 +0530

    card data

--- CreditCard.java
+++ CreditCard.java
@@ -12,6 +12,29 @@ public class CreditCard{
         bank = bk;
         account = acnt;
         limit = lim;
-        
+        balance = initialBal;
+    }
+    public CreditCard(String cust, String bk, String acnt, int lim){
+        this(cust, bk, acnt, lim, 0.0);
+    }
+
+    //methods
+    public String getCustomer(){return customer;}
+    pubi String getBank(){return bank;}
+    public String getAccount(){return account;}
+    public int getLimit(){return limit;}
+    public double getBalance(){return balance;}
+
+    //methods updates
+    public boolean charge(double price){
+        if(price + balance > limit)
+        return true;
+    }
+    public void makePayment(double amout){
+        balance -= amount;
+    }
+    //method to print account information
+    public static void printSummary(CreditCard card){
+        System.out.println("Customer = " + card.customer);
     }
 }
