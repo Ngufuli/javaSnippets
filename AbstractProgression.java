9e7b9a8cf555df4d668ffa2bc93cb1a14136554f
Ngufuli <ngufuli@outlook.com>
2023-10-26 14:55:45 +0530

    abstract

---
+++ AbstractProgression.java
@@ -0,0 +1,28 @@
+public abstract class AbstractProgression {
+    protected long current;
+
+    public AbstractProgression() {
+        this(0);
+    }
+
+    public AbstractProgression(long start) {
+        current = start;
+    }
+
+    public long nextValue() {
+        long answer = current;
+        advance();
+        return answer;
+    }
+
+    public void printProgression(int n){
+        System.out.print(nextValue());
+        for(int j=1; j<n; j++){
+            System.out.print(" " + nextValue())
+
+        }
+        System.out.Println();
+    }
+
+    protected abstract void advance();
+}
