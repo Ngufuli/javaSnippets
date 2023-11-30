44b263d105109fd13171a51f15f2c62dae764967
Ngufuli <ngufuli@outlook.com>
2023-12-01 02:16:21 +0530

    Generics

---
+++ ObjecPair.java
@@ -0,0 +1,17 @@
+public class ObjectPair {
+    Object first;
+    Object second;
+
+    public ObjectPair(Object a, Object b) { // constructor
+        first = a;
+        second = b;
+    }
+
+    public Object getFirst() {
+        return first;
+    }
+
+    public Object getSecond() {
+        return second;
+    }
+}
